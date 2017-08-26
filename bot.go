2c0daa6137f6360bf9cbcde93d007c373176de3c
Leonid Shevtsov <leonid@shevtsov.me>
2017-08-26 09:00:14 +0300

    refactor database and bot to be heroku-compatible

--- bot.go
+++ bot.go
@@ -1,8 +1,12 @@
 package gtd_bot
 
 import (
+	"fmt"
 	"log"
+	"math/rand"
+	"net/http"
 	"os"
+	"time"
 
 	"github.com/go-pg/pg"
 	telegram "github.com/go-telegram-bot-api/telegram-bot-api"
@@ -11,15 +15,22 @@ import (
 func RunBot(db *pg.DB) {
 	bot, err := telegram.NewBotAPI(os.Getenv("BOT_TOKEN"))
 	if err != nil {
-		log.Fatalln(err)
+		panic(err)
 	}
 
-	bot.Debug = true
-	log.Printf("Authorized on account %s", bot.Self.UserName)
+	if os.Getenv("DEBUG") != "" {
+		bot.Debug = true
+		log.Printf("Authorized on account %s", bot.Self.UserName)
+	}
 
-	u := telegram.NewUpdate(0)
-	u.Timeout = 60
-	updates, err := bot.GetUpdatesChan(u)
+	var updates telegram.UpdatesChannel
+
+	if webhookURL := os.Getenv("WEBHOOK_URL"); webhookURL != "" {
+		updates = runWebhook(bot, webhookURL)
+	} else {
+		updates = runLongPoll(bot)
+	}
+
 	for update := range updates {
 		if update.Message == nil {
 			continue
@@ -27,4 +38,42 @@ func RunBot(db *pg.DB) {
 		interaction := newInteraction(db, bot, update.Message)
 		interaction.handleMessage()
 	}
+}
+
+func runWebhook(bot *telegram.BotAPI, webhookURL string) telegram.UpdatesChannel {
+	rand.Seed(time.Now().UnixNano())
+	randomSeed := rand.Uint64()
+	webhookPath := fmt.Sprintf("/%d", randomSeed)
+	_, err := bot.SetWebhook(telegram.NewWebhook(webhookURL + webhookPath))
+	if err != nil {
+		panic(err)
+	}
+
+	updates := bot.ListenForWebhook(webhookPath)
+
+	var httpAddress string
+	if port := os.Getenv("WEBHOOK_PORT"); port != "" {
+		httpAddress = ":" + port
+	} else {
+		httpAddress = ":80"
+	}
+	go http.ListenAndServe(httpAddress, nil)
+
+	return updates
+}
+
+func runLongPoll(bot *telegram.BotAPI) telegram.UpdatesChannel {
+	// delete webhook, otherwise you cannot long poll
+	_, err := bot.SetWebhook(telegram.NewWebhook(""))
+	if err != nil {
+		panic(err)
+	}
+	// start long poll
+	u := telegram.NewUpdate(0)
+	u.Timeout = 60
+	updates, err := bot.GetUpdatesChan(u)
+	if err != nil {
+		panic(err)
+	}
+	return updates
 }
--- db.go
+++ db.go
@@ -2,24 +2,28 @@ package gtd_bot
 
 import (
 	"log"
+	"os"
 	"time"
 
 	"github.com/go-pg/pg"
 )
 
 func DBConnect() *pg.DB {
-	db := pg.Connect(&pg.Options{
-		Database: "gtd_bot",
-		User:     "postgres",
-		Addr:     ":5433",
-	})
-	db.OnQueryProcessed(func(event *pg.QueryProcessedEvent) {
-		query, err := event.FormattedQuery()
-		if err != nil {
-			panic(err)
-		}
-		log.Printf("%s %s", time.Since(event.StartTime), query)
-	})
+	dbURL := os.Getenv("DATABASE_URL")
+	dbOptions, err := pg.ParseURL(dbURL)
+	if err != nil {
+		panic(err)
+	}
+	db := pg.Connect(dbOptions)
+	if os.Getenv("DEBUG") != "" {
+		db.OnQueryProcessed(func(event *pg.QueryProcessedEvent) {
+			query, err := event.FormattedQuery()
+			if err != nil {
+				panic(err)
+			}
+			log.Printf("%s %s", time.Since(event.StartTime), query)
+		})
+	}
 	// err := createSchema(db)
 	// if err != nil {
 	// 	panic(err)
@@ -27,12 +31,12 @@ func DBConnect() *pg.DB {
 	return db
 }
 
-func createSchema(db *pg.DB) error {
-	for _, model := range []interface{}{&InboxItem{}, &User{}, &Goal{}, &Action{}} {
-		err := db.CreateTable(model, nil)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
+// func createSchema(db *pg.DB) error {
+// 	for _, model := range []interface{}{&InboxItem{}, &User{}, &Goal{}, &Action{}} {
+// 		err := db.CreateTable(model, nil)
+// 		if err != nil {
+// 			return err
+// 		}
+// 	}
+// 	return nil
+// }
